0e28af4e13e402ac736080258959f92936c13280
Selman Berk Özkurt <44111838+SelmanB@users.noreply.github.com>
2021-06-10 20:11:20 +0300

    Update urls.py
    added comment post API and display comments URLs

--- restful/FXrates/urls.py
+++ restful/FXrates/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns=[
     path('api/<str:pair>/<str:freq>/<int:begin>/<int:till>.json',views.api,name='api'),
     path('index.html',views.index,name='index'),
+    path('postcomment.html',views.postcomment,name='postcomment'),
+    path('getcomments.html',views.getcomments,name='getcomments'),
 ]
