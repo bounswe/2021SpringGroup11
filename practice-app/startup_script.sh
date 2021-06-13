e1ccf8dc9b0d328083860013e83230c850d72530
bekirtatar <bekirr.tatar@gmail.com>
2021-06-13 19:21:30 +0300

    'docker fixed'

--- practice-app/startup_script.sh
+++ practice-app/startup_script.sh
@@ -2,4 +2,5 @@
 
 python3 manage.py makemigrations
 python3 manage.py migrate
+python3 manage.py initialize 10
 python3 manage.py runserver 0.0.0.0:8000
--- practice-app/stocks/models.py
+++ practice-app/stocks/models.py
@@ -29,9 +29,9 @@ class StockCandle(models.Model):
 
 class Comment(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    symbol = models.CharField(max_length=50)
-    user = models.CharField(max_length=100)
-    comment = models.CharField(max_length=1000)
+    symbol = models.CharField(default='', max_length=50)
+    user = models.CharField(default='', max_length=100)
+    message = models.CharField(default='', max_length=1000)
 
     class Meta:
         ordering = ['created']
