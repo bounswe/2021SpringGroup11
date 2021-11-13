8df151982e8f5af4d501ae8c903936d57da4be2f
“hazerank” <hazar.cakir@boun.edu.tr>
2021-11-13 04:20:19 +0300

    Main body done initially

--- source/android/Renaissance/lib/home_page.dart
+++ source/android/Renaissance/lib/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:portakal/my_colors.dart';
+import 'package:portakal/widget/course_container.dart';
+import 'package:portakal/widget/tag_container.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,45 +18,217 @@ class _HomePageState extends State<HomePage> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-            backgroundColor: MyColors.blue,
-            leading: Image(
-              image: AssetImage('assets/logoOnly.png'),
-            ),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.vertical(
-                bottom: Radius.circular(42),
+          appBar: AppBar(
+              backgroundColor: MyColors.blue,
+              leading: Image(
+                image: AssetImage('assets/logoOnly.png'),
               ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(42),
+                ),
+              ),
+              title: Container(
+                width: double.infinity,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 2,
+                  ),
+                ),
+                child: Center(
+                  child: TextField(
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.white,
+                        ),
+                        hintText: 'Find something',
+                        hintStyle: TextStyle(
+                          color: Colors.white,
+                          fontStyle: FontStyle.italic,
+                        ),
+                        border: InputBorder.none),
+                  ),
+                ),
+              )),
+          body: ListView(physics: BouncingScrollPhysics(), children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ElevatedButton(
+                  child: Text(
+                    'Popular',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(MyColors.lightYellow),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                  ),
+                  onPressed: () => null,
+                ),
+                ElevatedButton(
+                  child: Text(
+                    'For You',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(MyColors.lightYellow),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                  ),
+                  onPressed: () => null,
+                ),
+                ElevatedButton(
+                  child: Text(
+                    'New',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(MyColors.lightYellow),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                  ),
+                  onPressed: () => null,
+                ),
+              ],
             ),
-            title: Container(
-              width: double.infinity,
-              height: 40,
-              decoration: BoxDecoration(
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              Expanded(
+                child: ElevatedButton(
+                  child: Text(
+                    'Tags',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.orange),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        side: BorderSide(width: 3),
+                      ),
+                    ),
+                  ),
+                  onPressed: () => null,
+                ),
+              )
+            ]),
+            Container(
+                width: double.infinity,
                 color: Colors.transparent,
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(
-                  color: Colors.white,
-                  width: 2,
-                ),
-              ),
-              child: Center(
-                child: TextField(
-                  decoration: InputDecoration(
-                      prefixIcon: Icon(
-                        Icons.search,
-                        color: Colors.white,
-                      ),
-                      hintText: 'Find something',
-                      hintStyle: TextStyle(
-                        color: Colors.white,
-                        fontStyle: FontStyle.italic,
-                      ),
-                      border: InputBorder.none),
-                ),
-              ),
-            )),
-        body: Text("This is a dumb budy"),
-      ),
+                child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                    decoration: BoxDecoration(
+                        color: Color(0x99FFFFFF),
+                        borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const SizedBox(width: 6),
+                            TagContainer("hello", 1, 2.0),
+                            const SizedBox(width: 12),
+                            TagContainer("hello ", 1, 2.0),
+                            const SizedBox(width: 12),
+                            TagContainer(" hge", 1, 2.0)
+                          ],
+                        ),
+                        const SizedBox(height: 18),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Expanded(
+                                child: ElevatedButton(
+                                  child: Text(
+                                    'Paths',
+                                    style: TextStyle(color: Colors.black),
+                                  ),
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all(
+                                        Colors.orange),
+                                    shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(15),
+                                          side: BorderSide(width: 3)),
+                                    ),
+                                  ),
+                                  onPressed: () => null,
+                                ),
+                              )
+                            ]),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const SizedBox(width: 6),
+                            CourseContainer("hello hello hello hge", 1, 2.0),
+                            const SizedBox(width: 12),
+                            CourseContainer("hello hello hello hge", 1, 2.0),
+                            const SizedBox(width: 12),
+                            CourseContainer("hello hello hello hge", 1, 2.0)
+                          ],
+                        ),
+                        const SizedBox(height: 8),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const SizedBox(width: 6),
+                            CourseContainer("hello hello hello hge", 1, 2.0),
+                            const SizedBox(width: 12),
+                            CourseContainer("hello hello hello hge", 1, 2.0),
+                            const SizedBox(width: 12),
+                            CourseContainer("hello hello hello hge", 1, 2.0)
+                          ],
+                        ),
+                        const SizedBox(height: 8),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const SizedBox(width: 6),
+                            CourseContainer("hello hello hello hge", 1, 2.0),
+                            const SizedBox(width: 12),
+                            CourseContainer("hello hello hello hge", 1, 2.0),
+                            const SizedBox(width: 12),
+                            CourseContainer("hello hello hello hge", 1, 2.0)
+                          ],
+                        ),
+                        const SizedBox(height: 8),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const SizedBox(width: 6),
+                            CourseContainer("hello hello hello hge", 1, 2.0),
+                            const SizedBox(width: 12),
+                            CourseContainer("hello hello hello hge", 1, 2.0),
+                            const SizedBox(width: 12),
+                            CourseContainer("hello hello hello hge", 1, 2.0)
+                          ],
+                        ),
+                      ],
+                    ))),
+          ])),
     );
   }
 }
--- source/android/Renaissance/lib/my_colors.dart
+++ source/android/Renaissance/lib/my_colors.dart
@@ -1,51 +1,51 @@
 import 'package:flutter/material.dart';
 
 class MyColors {
-  static const MaterialColor lightGreen = MaterialColor(
-      0xff9ee97a,
-      <int, Color>{
-        50: Color(0xff9ee97a),
-        100: Color(0xff9ee97a),
-        200: Color(0xff9ee97a),
-        300: Color(0xff9ee97a),
-        400: Color(0xff9ee97a),
-        500: Color(0xff9ee97a)
-      }
-      );
+  //FFDE8A
 
-  static const MaterialColor blue = MaterialColor(
-      0xff70a9ff,
-      <int, Color>{
-        50: Color(0xff70a9ff),
-        100: Color(0xff70a9ff),
-        200: Color(0xff70a9ff),
-        300: Color(0xff70a9ff),
-        400: Color(0xff70a9ff),
-        500: Color(0xff70a9ff)
-      }
-  );
+  static const MaterialColor lightYellow =
+      MaterialColor(0xffFFDE8A, <int, Color>{
+    50: Color(0xffFFDE8A),
+    100: Color(0xffFFDE8A),
+    200: Color(0xffFFDE8A),
+    300: Color(0xffFFDE8A),
+    400: Color(0xffFFDE8A),
+    500: Color(0xffFFDE8A)
+  });
+  static const MaterialColor lightGreen =
+      MaterialColor(0xff9ee97a, <int, Color>{
+    50: Color(0xff9ee97a),
+    100: Color(0xff9ee97a),
+    200: Color(0xff9ee97a),
+    300: Color(0xff9ee97a),
+    400: Color(0xff9ee97a),
+    500: Color(0xff9ee97a)
+  });
 
-  static const MaterialColor red = MaterialColor(
-      0xfff25151,
-      <int, Color>{
-        50: Color(0xfff25151),
-        100: Color(0xfff25151),
-        200: Color(0xfff25151),
-        300: Color(0xfff25151),
-        400: Color(0xfff25151),
-        500: Color(0xfff25151)
-      }
-  );
+  static const MaterialColor blue = MaterialColor(0xff70a9ff, <int, Color>{
+    50: Color(0xff70a9ff),
+    100: Color(0xff70a9ff),
+    200: Color(0xff70a9ff),
+    300: Color(0xff70a9ff),
+    400: Color(0xff70a9ff),
+    500: Color(0xff70a9ff)
+  });
 
-  static const MaterialColor darkGray = MaterialColor(
-    0xff4c525c,
-    <int, Color>{
-      50: Color(0xff4c525c),
-      100: Color(0xff4c525c),
-      200: Color(0xff4c525c),
-      300: Color(0xff4c525c),
-      400: Color(0xff4c525c),
-      500: Color(0xff4c525c)
-    }
-    );
+  static const MaterialColor red = MaterialColor(0xfff25151, <int, Color>{
+    50: Color(0xfff25151),
+    100: Color(0xfff25151),
+    200: Color(0xfff25151),
+    300: Color(0xfff25151),
+    400: Color(0xfff25151),
+    500: Color(0xfff25151)
+  });
+
+  static const MaterialColor darkGray = MaterialColor(0xff4c525c, <int, Color>{
+    50: Color(0xff4c525c),
+    100: Color(0xff4c525c),
+    200: Color(0xff4c525c),
+    300: Color(0xff4c525c),
+    400: Color(0xff4c525c),
+    500: Color(0xff4c525c)
+  });
 }
