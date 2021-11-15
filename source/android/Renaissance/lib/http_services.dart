09f20a7da367a2483ba0b71411d8eb11b03dd8fd
“hazerank” <hazar.cakir@boun.edu.tr>
2021-11-15 19:01:08 +0300

    register endpoint fix

--- source/android/Renaissance/lib/http_services.dart
+++ source/android/Renaissance/lib/http_services.dart
@@ -19,14 +19,9 @@ class HttpService {
   Future<LoginResponse> login(String username, String password) async {
     // only JWT strings return from this endpoint.
     String url = baseUrl + "/authentication/login/";
-    Response res = await post(
-      Uri.parse(url),
-      headers: headers,
-      body: jsonEncode({
-        'username': username,
-        'password': password
-      })
-    );
+    Response res = await post(Uri.parse(url),
+        headers: headers,
+        body: jsonEncode({'username': username, 'password': password}));
     if (res.statusCode == 200) {
       return LoginResponse.fromJson(json.decode(res.body));
     } else if (res.statusCode == 400) {
@@ -38,22 +33,21 @@ class HttpService {
     }
   }
 
-  Future<bool> register(String email, String username, String firstname, String lastname, String password) async {
-    String url = baseUrl + '/authentication/register/';
-    Response res = await post(
-      Uri.parse(url),
-      headers: headers,
-      body: jsonEncode({
-        'email': email,
-        'username': username,
-        'firstname': firstname,
-        'lastname': lastname,
-        'password': password
-      })
-    );
+  Future<bool> register(String email, String username, String firstname,
+      String lastname, String password) async {
+    String url = baseUrl + '/authentication/signup/';
+    Response res = await post(Uri.parse(url),
+        headers: headers,
+        body: jsonEncode({
+          'email': email,
+          'username': username,
+          'firstname': firstname,
+          'lastname': lastname,
+          'password': password
+        }));
     if (res.statusCode == 200) {
       return true;
-    } else if(res.statusCode == 406) {
+    } else if (res.statusCode == 406) {
       throw Exception('User already exists with this username or password.');
     } else {
       throw Exception('UnknownError');
@@ -62,19 +56,15 @@ class HttpService {
 
   Future<bool> forgotPassword(String username) async {
     String url = baseUrl + '/authentication/forgot-password/';
-    Response res = await post(
-      Uri.parse(url),
-      body: jsonEncode({
-        'username': username
-      })
-    );
+    Response res =
+        await post(Uri.parse(url), body: jsonEncode({'username': username}));
     if (res.statusCode == 202) {
       return true;
     }
     return false;
   }
 
- /* Future<String> refreshToken() async {
+  /* Future<String> refreshToken() async {
     String url = baseUrl + '/authentication/refresh-token';
 
     Response res = await post(
@@ -86,17 +76,11 @@ class HttpService {
 
   Future<bool> banUser(String username) async {
     String url = baseUrl + '/authentication/ban-user';
-    Response res = await post(
-      Uri.parse(url),
-      headers: headers,
-      body: jsonEncode({
-        'username': username
-      })
-    );
+    Response res = await post(Uri.parse(url),
+        headers: headers, body: jsonEncode({'username': username}));
     if (res.statusCode == 200) {
       return true;
     }
     return false;
   }
-
 }
