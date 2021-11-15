cbf47af54a6aec55de2b31f42847204dfea22cbf
SelmanB <SelmanB@github.com>
2021-11-15 17:11:17 +0300

    Added (redundant) "delete-user" endpoint to fit the specification.

--- source/backend/api/user/views.py
+++ source/backend/api/user/views.py
@@ -22,9 +22,32 @@ class EditUser(APIView):
 
         if not user:
             return Response({'detail': 'NO_USER'}, status=status.HTTP_400_BAD_REQUEST)
-        
+
         for key, value in data.items():
             user[key] = value
+
+        user = User(**user)
+        user.update()
+
+        return Response(user.get_dict(), status=status.HTTP_200_OK)
+
+class DeleteUser(APIView):
+    """
+        Delete User Class
+    """
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        data = request.data
+
+        with MongoDBHelper(uri=settings.MONGO_URI, database=settings.DB_NAME) as db:
+            user = db.find_one('user', query={'email': data['email'], 'username': data['username']})
+
+        if not user:
+            return Response({'detail': 'NO_USER'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user['isDeleted'] = True
 
         user = User(**user)
         user.update()
@@ -36,13 +59,25 @@ class SearchUser(APIView):
     """
         Search User Class
     """
+
     def get(self, request, search_text):
         with MongoDBHelper(uri=settings.MONGO_URI, database=settings.DB_NAME) as db:
+            db.create_index(
+                'user',
+                {'username': 'text'}
+            )
             users = db.find(
-                'user', # TODO Create fulltext index on username
-                query={'$or': [{'$text': {'$search': search_text}}, {'username': {'$regex': search_text, '$options': 'i'}}]},
+                'user',
+                query={'$or': [{'$text': {'$search': search_text}},
+                               {'username': {'$regex': search_text, '$options': 'i'}}],
+                       'isDeleted': False},
                 projection={'username': 1}
             )
+
+            # Alternative exclusion for deleted users
+            #for user in users:
+            #    if users[user].get('isDeleted'):
+            #        del users[user]
 
         return Response(users, status=status.HTTP_200_OK)
 
@@ -70,25 +105,26 @@ class BanUser(APIView):
         return Response({'detail': f'User with username: {user.username} banned successfully'},
                         status=status.HTTP_200_OK)
 
+
 class GetProfile(APIView):
 
     def get(self, request, username):
         with MongoDBHelper(uri=settings.MONGO_URI, database=settings.DB_NAME) as db:
             user = db.find_one(
-                    'user',
-                    query={'username': username},
-                    projection={
-                        '_id': 0,
-                        'updatedAt': 0,
-                        'password': 0,
-                        'lastLogin': 0,
-                        'rememberMe': 0, 
-                    }
-                )
+                'user',
+                query={'username': username},
+                projection={
+                    '_id': 0,
+                    'updatedAt': 0,
+                    'password': 0,
+                    'lastLogin': 0,
+                    'rememberMe': 0,
+                }
+            )
 
             if not user or user['isBanned'] or user.get('isDeleted'):
                 return Response('USER_NOT_FOUND', status=status.HTTP_404_NOT_FOUND)
-        
+
         return Response(user, status=status.HTTP_200_OK)
 
 
