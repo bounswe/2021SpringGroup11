fd61d4db6ff2d490bd6fbd6dcf493627ff47c477
SelmanB <SelmanB@github.com>
2021-12-12 20:11:49 +0300

    Typo fix throw->raise

--- source/backend/api/common/topicname.py
+++ source/backend/api/common/topicname.py
@@ -5,5 +5,5 @@ def topicname(id:int):
     url=f'https://www.wikidata.org/w/api.php?action=wbgetentities&props=labels&ids=Q{id}&languages=en&format=json'
     resp=requests.get(url)
     if not rep.ok:
-        throw ConnectionError("Error fetching topic name from Wikidata API.")
+        raise ConnectionError("Error fetching topic name from Wikidata API.")
     return resp.json()["entities"][f'Q{id}']["labels"]["en"]["value"]
