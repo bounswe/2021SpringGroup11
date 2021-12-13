bc2e5daee887998ab7b1bd472c3af28bd5efa4d8
Emin Deniz <emindeniz99@gmail.com>
2021-12-14 01:30:01 +0300

    feat(fe): backend connection and searchbox style
    react-toastify
    
    Co-authored-by: meltemarsl <meltemmarslannn@gmail.com>

--- source/frontend/src/components/Topic/fakeData.ts
+++ source/frontend/src/components/Topic/fakeData.ts
@@ -5,9 +5,10 @@ export const getFakeTopics = (count = 1000, fixedValues: Partial<ITopic> = {}) =
   const mocktopicdata: ITopic[] = [];
   for (let index = 0; index < count; index += 1) {
     mocktopicdata.push({
-      ID: faker.datatype.number(),
+      id: faker.datatype.number(),
       name: Math.random() < 0.5 ? faker.random.words(1) : faker.random.words(2),
       isFav: faker.datatype.boolean(),
+      description: faker.lorem.paragraph(),
       ...fixedValues,
     });
   }
@@ -18,13 +19,21 @@ export const getFakePaths = (count = 1000, fixedValues: Partial<ITopic> = {}) =>
   const mockpathdata: IPath[] = [];
   for (let index = 0; index < count; index += 1) {
     mockpathdata.push({
-      ID: faker.datatype.number(),
+      _id: faker.datatype.number(),
       title: Math.random() < 0.5 ? faker.random.words(3) : faker.random.words(5),
       photo: faker.image.imageUrl(64, 64, undefined, true),
       effort: faker.datatype.number(10),
       rating: faker.datatype.number(10),
       isEnrolled: faker.datatype.boolean(),
       isFav: faker.datatype.boolean(),
+      description: faker.lorem.paragraph(),
+      created_at: faker.datatype.number(10),
+      creator_username: faker.datatype.string(10),
+      isFollowed: faker.datatype.boolean(),
+      is_banned: false,
+      is_deleted: false,
+      milestones: [],
+      topics: [],
       ...fixedValues,
     });
   }
