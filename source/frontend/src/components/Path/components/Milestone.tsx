95adae2db0db3b28a767cf1673ef582e45751ca9
Bekir Tatar <bekir@shopihq.com>
2021-12-14 11:52:33 +0300

    feat(path-page):
    Path page is implemented. Get-path and enroll path functionalities are working properly now.
    
    BREAKING CHANGE: ENROLL PATH

---
+++ source/frontend/src/components/Path/components/Milestone.tsx
@@ -0,0 +1,65 @@
+import { makeStyles } from '@mui/styles';
+import { Button, Checkbox, CircularProgress, Slide, Typography } from '@mui/material';
+import * as React from 'react';
+interface Props {
+  title: any;
+  body: string;
+  isEnrolled: boolean;
+  isChecked: boolean;
+  loading: boolean;
+  // eslint-disable-next-line no-unused-vars
+  onClick(value: boolean): any;
+}
+
+const useStyles = makeStyles(() => ({
+  root: {
+    width: '100%',
+    display: 'flex',
+    padding: '100',
+    flexDirection: 'column',
+    boxSizing: 'border-box',
+    border: '2px solid #955B9DFF',
+    borderRadius: '3px',
+    marginTop: 10,
+  },
+  item: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  body: {
+    padding: 10,
+  },
+}));
+const Milestone = (props: Props) => {
+  const { title, body, isEnrolled, isChecked = false, loading, onClick } = props;
+  const [showBody, setShowBody] = React.useState(false);
+  const [checked, setChecked] = React.useState(isChecked);
+  const handleShowBody = () => {
+    setShowBody(!showBody);
+  };
+  const handleClick = (event) => {
+    event.persist();
+    onClick(!checked);
+    setChecked(!checked);
+  };
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <div className={classes.item}>
+        <Button onClick={handleShowBody}>
+          <Typography>{title}</Typography>
+        </Button>
+        {isEnrolled &&
+          (loading ? <CircularProgress /> : <Checkbox checked={checked} onChange={handleClick} />)}
+      </div>
+      {showBody && (
+        <Slide direction="up" in={showBody} mountOnEnter unmountOnExit>
+          <Typography className={classes.body}>{body}</Typography>
+        </Slide>
+      )}
+    </div>
+  );
+};
+
+export default Milestone;
---
+++ source/frontend/src/components/Path/reducer.ts
@@ -0,0 +1,60 @@
+import produce from 'immer';
+
+import {
+  DEFAULT_ACTION,
+  GET_PATH,
+  GET_PATH_FAILURE,
+  GET_PATH_SUCCESS,
+  ENROLL_PATH,
+  ENROLL_PATH_SUCCESS,
+  ENROLL_PATH_FAILURE,
+} from './constants';
+
+export const initialState = {
+  loading: true,
+  pathId: '',
+  path: undefined,
+  error: undefined,
+  enrollRes: undefined,
+  enrollError: undefined,
+};
+
+const pathReducer = (state = initialState, action) =>
+  produce(state, (draft) => {
+    switch (action.type) {
+      case DEFAULT_ACTION:
+        break;
+      case GET_PATH:
+        draft.loading = true;
+        draft.pathId = action.pathId;
+        draft.path = undefined;
+        break;
+      case GET_PATH_SUCCESS:
+        draft.loading = false;
+        draft.path = action.path;
+        draft.error = undefined;
+        break;
+      case GET_PATH_FAILURE:
+        draft.loading = false;
+        draft.error = action.error;
+        break;
+      case ENROLL_PATH:
+        draft.loading = true;
+        draft.pathId = action.pathId;
+        draft.enrollError = undefined;
+        break;
+      case ENROLL_PATH_SUCCESS:
+        draft.loading = false;
+        draft.enrollRes = action.response;
+        draft.enrollError = undefined;
+        break;
+      case ENROLL_PATH_FAILURE:
+        draft.loading = false;
+        draft.enrollError = action.error;
+        break;
+      default:
+        break;
+    }
+  });
+
+export default pathReducer;
