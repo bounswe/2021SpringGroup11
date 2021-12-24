2c2ffe572b36c8838f7b7ea68befb8320b20e731
EAltunoglu <erenaltunoglu@gmail.com>
2021-12-25 00:00:19 +0300

    Get request test case added

--- source/backend/api/user/tests.py
+++ source/backend/api/user/tests.py
@@ -17,12 +17,24 @@ DELETEDUSER_JWT_DATA = {
     'exp': int(time.time()) + 60*60*24
 }
 
+ADMINUSER_JWT_DATA = {
+    'username': "test_admin",
+    'email': "test@test.com",
+    'isAdmin': True,
+    'exp': int(time.time()) + 60*60*24
+}
+
+
 AUTHENTICATED_NORMALUSER_HEADERS = {
     "HTTP_AUTHORIZATION": create_jwt(NORMALUSER_JWT_DATA),
 }
 
 AUTHENTICATED_DELETEDUSER_HEADERS = {
     "HTTP_AUTHORIZATION": create_jwt(DELETEDUSER_JWT_DATA),
+}
+
+AUTHENTICATED_NORMALUSER_HEADERS = {
+    "HTTP_AUTHORIZATION": create_jwt(ADMINUSER_JWT_DATA),
 }
 
 UNAUTHENTICATED_NORMALUSER_HEADERS = {
@@ -68,3 +80,33 @@ class EditUserTest(TestCase):
         )
         
         self.assertEquals(response.status_code, 400)
+
+
+class SearchUserTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_200_code(self):
+        response = self.client.get(
+            reverse('search_user', kwargs={'search_text': 'test'})
+        )
+
+        self.assertEquals(response.status_code, 200)
+
+"""
+class BanUserTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+    
+    def test_200_code(self):
+        data = {'username': 'banned_user'}
+    
+        response = self.client.post(
+            reverse('ban_user'),
+            data,
+            **AUTHENTICATED_NORMALUSER_HEADERS,
+            content_type="application/json"
+        )
+        
+        self.assertEquals(response.status_code, 200)
+"""
--- source/backend/api/user/urls.py
+++ source/backend/api/user/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('edit-user/', views.EditUser.as_view(), name='edit_user'),
-    path('search-user/<slug:search_text>/', views.SearchUser.as_view(), name='search-user'),
-    path('ban-user/', views.BanUser.as_view(), name='ban-user'),
+    path('search-user/<slug:search_text>/', views.SearchUser.as_view(), name='search_user'),
+    path('ban-user/', views.BanUser.as_view(), name='ban_user'),
     path('get-profile/<slug:username>/', views.GetProfile.as_view(), name='get-profile'),
     path('change-password/', views.ChangePassword.as_view(), name='change-password'),
     #path('get-details/', views.GetDetails.as_view(), name='get-details'),
