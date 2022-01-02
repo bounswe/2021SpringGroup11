195e93b370b4fcc30a849bcf8b2453a51c0dd0c0
SelmanB <SelmanB@github.com>
2022-01-02 21:05:39 +0300

    typo fixes in activitystreams.py

--- source/backend/api/common/activitystreams.py
+++ source/backend/api/common/activitystreams.py
@@ -3,7 +3,7 @@ from datetime import datetime as dt
 
 def activity_format(summary:str, username:str, obj_id:str, obj_name:str, action="Create", aux=None):
 
-  res={
+    res={
         "@context":"https://www.w3.org/ns/activitystreams",
         "summary":summary,
         "type": action,
@@ -15,7 +15,7 @@ def activity_format(summary:str, username:str, obj_id:str, obj_name:str, action=
             "id":obj_id,
             "type":"Document",
             "name":obj_name
-        }
+        },
         "published":dt.fromtimestamp(time.time()).isoformat()
     }
     if aux:
